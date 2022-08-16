9b10272b68cbced3cc5a805ef646b2f1cbdd5ef6
Vladimir <falinvlad93@rambler.ru>
2022-08-16 20:40:05 +0300

    fix img

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,3 +1,5 @@
+import unknownHero from '../../assets/unknown-hero.jpg'
+
 const HeroesListItem = ({name, description, element, onDelete}) => {
 
     let elementClassName;
@@ -22,7 +24,7 @@ const HeroesListItem = ({name, description, element, onDelete}) => {
     return (
         <li 
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
+            <img src={unknownHero} 
                  className="img-fluid w-25 d-inline" 
                  alt="unknown hero" 
                  style={{'objectFit': 'cover'}}/>
